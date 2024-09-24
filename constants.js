4102ed43d51e3ec6eaa2b59f69d67f894d33436f
= <=>
2024-09-24 16:37:33 -0400

    I still struggle with simple stuff

--- constants.js
+++ constants.js
@@ -1,52 +1,56 @@
 const HEIGHT = window.innerHeight;
 const WIDTH = window.innerWidth;
 
+const isProduction = window.location.hostname === "ryanlarge13.github.io";
+const repoName = "/Altos-Odyssey-Clone";
+const baseURL = isProduction ? window.location.hostname + repoName : "";
+
 export const srcs = [
   {
     name: "sky",
-    src: "../assets/images/sky.png",
+    src: `${baseURL}/assets/images/sky.png`,
     initializedOffset: { x: 0, y: HEIGHT * 0.6 },
     translation: { x: false, y: true },
   },
   {
     name: "sun",
-    src: "../assets/images/sun.png",
+    src: `${baseURL}/assets/images/sun.png`,
     initializedOffset: { x: WIDTH / 4, y: 0 },
     translation: { x: false, y: false },
   },
   {
     name: "clouds-back",
-    src: "../assets/images/clouds-back.png",
+    src: `${baseURL}/assets/images/clouds-back.png`,
     initializedOffset: { x: 0, y: 0 },
     translation: { x: false, y: false },
   },
   {
     name: "clouds-front",
-    src: "../assets/images/clouds-front.png",
+    src: `${baseURL}/assets/images/clouds-front.png`,
     initializedOffset: { x: 0, y: 0 },
     translation: { x: false, y: false },
   },
   {
     name: "bg1",
-    src: "../assets/images/bg-1.png",
+    src: `${baseURL}/assets/images/bg-1.png`,
     initializedOffset: { x: 0, y: HEIGHT },
     translation: { x: false, y: true },
   },
   {
     name: "bg2",
-    src: "../assets/images/bg-2.png",
+    src: `${baseURL}/assets/images/bg-2.png`,
     initializedOffset: { x: 0, y: HEIGHT + 50 },
     translation: { x: false, y: true },
   },
   {
     name: "bg3",
-    src: "../assets/images/bg-3.png",
+    src: `${baseURL}/assets/images/bg-3.png`,
     initializedOffset: { x: 0, y: HEIGHT },
     translation: { x: false, y: true },
   },
   {
     name: "boarder",
-    src: "../assets/images/boarder.png",
+    src: `${baseURL}/assets/images/boarder.png`,
     initializedOffset: { x: 200, y: HEIGHT - 200 },
     translation: { x: false, y: false },
   },
